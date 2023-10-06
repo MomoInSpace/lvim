a7fe1e7c9a7e273fe27e22ee3edbc9131a00039e
MomoInSpace <unwissendweilmensch@gmx.de>
2023-10-06 11:30:42 +0200

    Added Better LuaSnips Keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -20,7 +20,7 @@ lvim.builtin.which_key.mappings["t"] = {
 -----------------------------------------------------------
 
 local map = vim.api.nvim_set_keymap
-local default_opts = {noremap = true, silent = true}
+local default_opts = { noremap = true, silent = true }
 
 -----------------------------------------------------------
 -- Neovim shortcuts:
@@ -61,3 +61,141 @@ map('t', '<Esc>', '<C-\\><C-n>', default_opts)
 -- Move Between Tabs
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+
+
+
+
+-- Settings for nvim-cmp
+
+local cmp = require("cmp")
+local types = require("cmp.types")
+local str = require("cmp.utils.str")
+
+local t = function(str)
+    return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+local luasnip = require("luasnip")
+
+-- Do not jump to snippet if i'm outside of it
+-- https://github.com/L3MON4D3/LuaSnip/issues/78
+-- luasnip.config.setup({
+-- 	region_check_events = "CursorMoved",
+-- 	delete_check_events = "TextChanged",
+-- })
+
+-- local lspkind = require("lspkind")
+-- local neogen = require("neogen")
+
+-- Keymaps for LuaSnips
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+        end,
+    },
+    mapping = {
+        ["<C-j>"] = cmp.mapping(
+            cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+            { "i", "s", "c" }
+        ),
+        ["<C-k>"] = cmp.mapping(
+            cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+            { "i", "s", "c" }
+        ),
+        -- ["<Tab>"] = cmp.mapping(function(fallback)
+        -- 	-- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
+        -- 	if cmp.visible() then
+        -- 		local entry = cmp.get_selected_entry()
+        -- 		if not entry then
+        -- 			cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+        -- 		end
+        -- 		cmp.confirm()
+        -- 	else
+        -- 		fallback()
+        -- 	end
+        -- end, {
+        -- 	"i",
+        -- 	"s",
+        -- 	"c",
+        -- }),
+        ["<C-l>"] = cmp.mapping(function(fallback)
+            if luasnip.expand_or_jumpable() then
+                vim.fn.feedkeys(t("<Plug>luasnip-expand-or-jump"), "")
+            elseif neogen.jumpable() then
+                vim.fn.feedkeys(t("<cmd>lua require('neogen').jump_next()<CR>"), "")
+            else
+                fallback()
+            end
+        end, {
+            "i",
+            "s",
+        }),
+        ["<C-h>"] = cmp.mapping(function(fallback)
+            if luasnip.jumpable(-1) then
+                vim.fn.feedkeys(t("<Plug>luasnip-jump-prev"), "")
+            else
+                fallback()
+            end
+        end, {
+            "i",
+            "s",
+        }),
+        ["<C-a>"] = cmp.mapping(function(fallback)
+            if luasnip.choice_active() then
+                vim.fn.feedkeys(t("<Plug>luasnip-next-choice"), "")
+            else
+                fallback()
+            end
+        end, {
+            "i",
+            "s",
+        }),
+        ["<C-s>"] = cmp.mapping(function(fallback)
+            if luasnip.choice_active() then
+                vim.fn.feedkeys(t("<Plug>luasnip-prev-choice"), "")
+            else
+                fallback()
+            end
+        end, {
+            "i",
+            "s",
+        }),
+    },
+
+    -- You should specify your *installed* sources.
+    -- sources = {
+    --     { name = "cmp_git" },
+    --     { name = "path" },
+    --     { name = "luasnip" },
+    --     { name = "nvim_lsp" },
+    --     { name = "buffer",  keyword_length = 5, max_item_count = 5 },
+    --     { name = "neorg" },
+    -- },
+
+    -- formatting = {
+    --     -- Youtube: How to set up nice formatting for your sources.
+    --     format = lspkind.cmp_format {
+    --         with_text = true,
+    --         menu = {
+    --             buffer = "[buf]",
+    --             nvim_lsp = "[LSP]",
+    --             nvim_lua = "[api]",
+    --             path = "[path]",
+    --             luasnip = "[snip]",
+    --             gh_issues = "[issues]",
+    --             tn = "[TabNine]",
+    --         },
+    --     },
+    -- },
+
+    -- experimental = {
+    --     ghost_text = false,
+    -- },
+})
+
+require("cmp").setup.cmdline(":", {
+    sources = {
+        { name = "cmdline", keyword_length = 2 },
+    },
+})
