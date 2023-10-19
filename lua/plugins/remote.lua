1407bcd3156c5b497ff6c81d50c1947ff72bebba
MomoInSpace <unwissendweilmensch@gmx.de>
2023-10-19 11:17:16 +0200

    Burgixed the remote plugin

--- lua/plugins/remote.lua
+++ lua/plugins/remote.lua
@@ -1,5 +1,5 @@
--- "NOSDuco/remote-sshfs.nvim" lets you connect nvim to a remote ssh 
--- connection so that you don't have to instal nvim there. 
+-- "NOSDuco/remote-sshfs.nvim" lets you connect nvim to a remote ssh
+-- connection so that you don't have to instal nvim there.
 -- https://github.com/NOSDuco/remote-sshfs.nvim
 
 require('remote-sshfs').setup({})
